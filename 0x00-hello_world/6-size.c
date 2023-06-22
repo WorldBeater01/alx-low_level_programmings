f9637fbd3428db74e71cdc807726596b5ac1af06
Isaac Badiru <pelumibadiru@gmail.com>
2023-06-22 12:54:05 -0700

    Size Matters

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+	long int li;
+	long long int lli;
+	float f;
+		printf("Size of a char: %zu byte(s)\n", sizeof(c));
+		printf("Size of a int: %zu byte(s)\n", sizeof(i));
+		printf("Size of a long int: %zu byte(s)\n", sizeof(li));
+		printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
+		printf("Size of a float: %zu byte(s)\n", sizeof(f));
+	return (0);
+}
