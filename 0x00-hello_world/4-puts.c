849c9426fb0bb955424c65ee306804e606ed6fb8
Isaac Badiru <pelumibadiru@gmail.com>
2023-06-22 11:23:02 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * This is the main function.
+ * It serves as the entry point of the program.
+ */
+int main(void)
+{
+	const char *message = "Programming is like building a multilingual puzzle";
+		puts(message);
+	return (0);
+}
