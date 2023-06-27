17d1947697007b3a6f0104d7cf0f3744681b8b8b
Isaac Badiru <pelumibadiru@gmail.com>
2023-06-27 07:29:28 -0700

    Debug that babd

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int positive_or_negative()
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if (n > 0)
+        {
+                printf("%d is positive\n", n);
+        }
+        else if (n < 0)
+        {
+                printf("%d is negative\n", n);
+        }
+        else
+        {
+                printf("%d is zero\n", n);
+        }
+        return (0);
+}
