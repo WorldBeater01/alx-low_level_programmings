5d6fabf6cf61ba252dd9570ffdc349c8ae4d368b
Isaac Badiru <pelumibadiru@gmail.com>
2023-06-28 03:04:20 -0700

    Square Up

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - Prints a square
+ * @size: Size of square
+ *
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			putchar('#');
+		}
+		putchar ('\n');
+	}
+}
