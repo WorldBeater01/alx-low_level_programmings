7c97b6edc58511e1f2e80491ddb7e3e39b5e3982
Isaac Badiru <pelumibadiru@gmail.com>
2023-06-28 01:12:01 -0700

    Manutd Liv *2

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - Prints 0 times the numbers, from 0 to 14.
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+		for (i = 0; i <= 10; i++)
+		{
+			for (j = 0; j <= 14; j++)
+			{
+				if (j > 9)
+				{
+					putchar('1');
+				}
+				putchar('0' + (j % 10));
+			}
+			putchar('\n');
+		}
+}
